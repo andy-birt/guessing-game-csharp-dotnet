cd02fd7c7dbf9234d2f9d8a4faffb3732e679297
Andrew Birt <abirt1986@gmail.com>
2022-01-20 15:33:20 -0500

    complete phase 2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace GuessingGame
   {
     static void Main(string[] args)
     {
+      int secretNumber = 42;
       //* Initialize guess to be "null" to begin executing while
       //? The question mark indicates that the string can be nullable
       //? Without it a warning is present  
@@ -19,7 +20,14 @@ namespace GuessingGame
         guess = Console.ReadLine();
       }
       
-      Console.WriteLine($"You guessed {guess}");
+      if (secretNumber == num)
+      {
+        Console.WriteLine($"NOICE! it totally is {guess}!");
+      }
+      else
+      {
+        Console.WriteLine($"WROOONNNG! it is {secretNumber}!");
+      }
     }
   }
 }
