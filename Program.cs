30ea292e0208516a5cc766e0821ef2b6f9269268
Andrew Birt <abirt1986@gmail.com>
2022-01-20 14:41:34 -0500

    from .net6 to .net5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GuessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("hello world");
+    }
+  }
+}
